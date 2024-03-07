e7a4040baf135412789a3f81e8d4c3852e043d81
jefferson hideyuki okuda <126090620+jeffersonDMX@users.noreply.github.com>
2024-03-06 21:07:40 -0300

    Add files via upload

---
+++ 1_tipos_primitivos/Untitled-2.js
@@ -0,0 +1,10 @@
+const texto1 = "kaike cupin";
+const texto = "gabizzin";
+
+const texto3 = 'o gabizzin foi no banheiro" ';
+
+console.log(texto1);
+console.log(texto2);
+console.log(texto3);
+
+console.log(texto2 + texto3);
