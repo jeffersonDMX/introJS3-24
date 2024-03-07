1c67bef7a883873d09256da10d27b1d88ec19306
jefferson hideyuki okuda <126090620+jeffersonDMX@users.noreply.github.com>
2024-03-06 21:07:09 -0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,17 @@
+// tipo number
+
+const meuNUmero = 24;
+
+const telefone = 44997116881
+
+const idade = 29;
+
+
+console.log(meuNUmero);
+console.log(telefone);
+console.log(idade);
+
+
+const soma = meuNUmero + idade;
+
+console.log(soma)
