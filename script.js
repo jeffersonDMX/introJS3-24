4c02f225f7745527029b78e710513f666e2e63ec
jefferson hideyuki okuda <126090620+jeffersonDMX@users.noreply.github.com>
2024-03-15 22:19:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const text1  = "olá mundo"; 
+const text2  = "olá mundo";
+const senha  = "senhasupersegura";
+const stringdenumeros = "3642";
+
+//contatenacao (+)
+
+const citacao = "meu nome é";
+const meunome = "jefferson hideyuki okuda";
+console.log(citacao + meunome)
